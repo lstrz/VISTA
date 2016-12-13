f53b1a53f06f4ec5b1abd87c3de773828acfd31e
lstrz <l@l.l>
2016-12-13 14:59:59 +0100

    Reverted from a test.

--- VISTA.ino
+++ VISTA.ino
@@ -55,21 +55,16 @@ void LSM303init(){
     acce.enableDefault();
     if(acce.last_status) notOkStop();
     else ok();
-    
-    Serial.print(" - setting data rate to 1600Hz from 50Hz: ");
-    acce.writeReg(LSM303::CTRL_REG1_A, 0xA7);
+      
+    Serial.print(" - setting data rate to 12.5Hz from 50Hz: ");
+    acce.writeReg(LSM303::CTRL_REG1_A, 0x37);
     if(acce.last_status) notOkStop();
     else ok();
     
-//    Serial.print(" - setting data rate to 12.5Hz from 50Hz: ");
-//    acce.writeReg(LSM303::CTRL_REG1_A, 0x37);
-//    if(acce.last_status) notOkStop();
-//    else ok();
-    
-//    Serial.print(" - setting range to +-16g from +-2g: ");
-//    acce.writeReg(LSM303::CTRL_REG2_A, 0x20);
-//    if(acce.last_status) notOkStop();
-//    else ok();
+    Serial.print(" - setting range to +-16g from +-2g: ");
+    acce.writeReg(LSM303::CTRL_REG2_A, 0x20);
+    if(acce.last_status) notOkStop();
+    else ok();
     
   }else{
     notOkStop();
@@ -101,24 +96,27 @@ void setup() {
     delay(1000);
   }
 
-  //L3Ginit();
+  L3Ginit();
   LSM303init();
-  //LPSinit();
+  LPSinit();
 }
 
 void loop() {
-//  Serial.print(pres.pressureToAltitudeMeters(pres.readPressureMillibars(), 1006));
-//  Serial.print("\n");
-  
+  Serial.println("Altimeter");
+  Serial.print(pres.pressureToAltitudeMeters(pres.readPressureMillibars(), 1006));
+  Serial.print("\n");
+
+  Serial.println("Accelerometer");
   acce.read();
   Serial.println(acce.a.x);
   Serial.println(acce.a.y);
   Serial.println(acce.a.z);
 
-//  gyro.read();
-//  Serial.println(gyro.g.x);
-//  Serial.println(gyro.g.y);
-//  Serial.println(gyro.g.z);
+  Serial.println("Gyroscope");
+  gyro.read();
+  Serial.println(gyro.g.x);
+  Serial.println(gyro.g.y);
+  Serial.println(gyro.g.z);
   
-  delay(1);
+  delay(100);
 }
